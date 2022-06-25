46a543c6e679a247ebf18105c0fa2360cef273ac
Farzana Anjum <30755050+farzanaanjum@users.noreply.github.com>
2022-06-25 07:43:57 +0530

    palindrome string
    palindrome string

---
+++ Python-Problems/Palindrome-String.py
@@ -0,0 +1,10 @@
+## Check a string is palindrome
+
+string = input(("Enter a string: "))
+
+if(string == string[::-1]):
+    print("The string is a palindrome")
+else:
+    print("Not a palindrome")
+
+    
