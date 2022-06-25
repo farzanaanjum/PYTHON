6dea0ea7ff15f176a6b96b4a228940bd8bc9002f
Farzana Anjum <30755050+farzanaanjum@users.noreply.github.com>
2022-06-25 22:10:47 +0530

    reverse string
    reverse string

---
+++ Loops-In-Python/For-Lopps/Reverse-the-String.py
@@ -0,0 +1,12 @@
+## Reverse the string in python
+
+str = input("Enter a String: ")
+
+for i in range(len(str) -1, -1, -1):
+	print(str[i], end = "")
+print("\n")
+
+
+
+# Input:    python
+# Output:   nohtyp
