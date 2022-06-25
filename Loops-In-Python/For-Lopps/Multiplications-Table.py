f8a9daf83f417a49e08edbbe862516b0624efebc
Farzana Anjum <30755050+farzanaanjum@users.noreply.github.com>
2022-06-25 22:09:18 +0530

    Multiplication Table
    Multiplication Table

---
+++ Loops-In-Python/For-Lopps/Multiplications-Table.py
@@ -0,0 +1,15 @@
+## Write a program to print multiplication table of a given number?
+
+n = 2
+
+# stop: 11 (because range never include stop number in result)
+# run loop 10 times
+
+for i in range(1, 11, 1):
+
+    # 2 * i (current number)
+    product = n * i
+    
+    print(product)
+    
+   
